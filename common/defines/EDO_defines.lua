c314e6ac712019b77dbd782d109cbc9451628f80
IndestructibleM <57876006+IndestructibleM@users.noreply.github.com>
2026-07-12 15:53:46 -0400

    Push

--- common/defines/EDO_defines.lua
+++ common/defines/EDO_defines.lua
@@ -1,5 +1,5 @@
-NDefines.NGame.START_DATE = "1936.1.1.12"
-NDefines.NGame.END_DATE = "1970.1.1.1"
+NDefines.NGame.START_DATE = "1925.1.1.12"
+NDefines.NGame.END_DATE = "1950.1.1.1"
 
 NDefines.NFrontend.CAMERA_MIN_HEIGHT = 50.0
 NDefines.NFrontend.CAMERA_MAX_HEIGHT = 3800.0
